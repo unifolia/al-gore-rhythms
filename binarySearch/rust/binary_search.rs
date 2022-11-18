95e94723c36b03c49c0eaf7b6b7d1daaca0b134f
James Lewis <james@Jamess-MacBook-Air.local>
2022-11-18 18:35:44 -0500

    Using rand generator for arrays in Rust

--- binarySearch/rust/binary_search.rs
+++ binarySearch/rust/binary_search.rs
@@ -1,22 +1,24 @@
-const SOME_ARRAY: [i32; 5] = [1, 2, 3, 4, 5];
+use rand::Rng;
 
 fn main() {
-    const NEEDLE: i32 = 2; // or whatever you're looking for
+    let mut rng = rand::thread_rng();
+    const SOME_ARRAY: [i32; 10] = [1, 3, 5, 6, 7, 9, 11, 14, 16, 20];
+    let needle: i32 = rng.gen_range(1..20); // or whatever you're looking for
     let mut low = 0;
     let mut high = SOME_ARRAY.len();
 
     while high > low {
         let middle = low + (high - low) / 2;
 
-        if SOME_ARRAY[middle] == NEEDLE {
-            println!("{} at index {}", NEEDLE, middle);
+        if SOME_ARRAY[middle] == needle {
+            println!("{} at index {}", needle, middle);
             return;
-        } else if SOME_ARRAY[middle] < NEEDLE {
+        } else if SOME_ARRAY[middle] < needle {
             low = middle + 1; // exclude middle as we've already looked there
-        } else {
+        } else { // if middle > needle
             high = middle;
         }
     };
-    println!("{} not in array", NEEDLE);
+    println!("{} not in array", needle);
     return
 }
